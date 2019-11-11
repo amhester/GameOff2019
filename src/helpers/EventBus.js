ed05198c9681e044e495d0a758e3c1fae292175d
David Ragsdale <me@davidjragsdale.com>
2019-11-11 07:47:03 -0500

    Set up standard eventing system

---
+++ src/helpers/EventBus.js
@@ -0,0 +1,11 @@
+import Phaser from 'phaser';
+
+let instance = null;
+export default class EventDispatcher extends Phaser.Events.EventEmitter {
+  static getInstance() {
+    if (instance == null) {
+      instance = new EventDispatcher();
+    }
+    return instance;
+  }
+}
--- src/scenes/SampleScene.js
+++ src/scenes/SampleScene.js
@@ -1,5 +1,6 @@
 import Phaser from 'phaser';
 import AlignGrid from '../helpers/AlignGrid';
+import EventBus from '../helpers/EventBus';
 
 import jeff from '../assets/jeff.png';
 
@@ -9,16 +10,14 @@ export default class SampleScene extends Phaser.Scene {
 
   constructor() {
     super('SampleScene');
+
+    this.EventBus = EventBus.getInstance();
   }
 
   preload() {
-    // this.load.setBaseURL('/dist');
     // this.load.setBaseURL('http://labs.phaser.io');
 
     this.load.image('jeff', jeff);
-    // this.load.image('sky', 'assets/skies/space3.png');
-    // this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-    // this.load.image('red', 'assets/particles/red.png');
   }
 
   create() {
@@ -29,19 +28,6 @@ export default class SampleScene extends Phaser.Scene {
     });
     this.grid.showNumbers(); // Comment out to make lines disappear
 
-    // this.add.image(400, 300, 'sky');
-
-    // const particles = this.add.particles('red');
-
-    // const emitter = particles.createEmitter({
-    //   speed: 100,
-    //   scale: {
-    //     start: 1,
-    //     end: 0,
-    //   },
-    //   blendMode: 'ADD',
-    // });
-
     this.jeff = this.physics.add.image(0, 0, 'jeff');
     this.grid.scaleToCell(this.jeff);
     this.grid.placeAtIndex(13, this.jeff);
@@ -50,30 +36,38 @@ export default class SampleScene extends Phaser.Scene {
     this.jeff.setBounce(1, 1);
     this.jeff.setCollideWorldBounds(true);
 
-    // this.#logo = this.physics.add.image(400, 100, 'logo');
+    this.EventBus.on('ARROW_LEFT', () => {
+      console.log('LEFT');
+      this.jeff.setVelocity(this.#VELOCITY_FACTOR * -1, 0);
+    });
 
-    // this.#logo.setVelocity(0, 0);
-    // this.#logo.setBounce(1, 1);
-    // this.#logo.setCollideWorldBounds(true);
+    this.EventBus.on('ARROW_RIGHT', () => {
+      console.log('RIGHT');
+      this.jeff.setVelocity(this.#VELOCITY_FACTOR * 1, 0);
+    });
 
-    // emitter.startFollow(this.#logo);
+    this.EventBus.on('ARROW_UP', () => {
+      console.log('UP');
+      this.jeff.setVelocity(0, this.#VELOCITY_FACTOR * -1);
+    });
+
+    this.EventBus.on('ARROW_DOWN', () => {
+      console.log('DOWN');
+      this.jeff.setVelocity(0, this.#VELOCITY_FACTOR * 1);
+    });
   }
 
   update() {
     const cursors = this.input.keyboard.createCursorKeys();
 
     if (cursors.left.isDown) {
-      console.log('LEFT');
-      this.jeff.setVelocity(this.#VELOCITY_FACTOR * -1, 0);
+      this.EventBus.emit('ARROW_LEFT');
     } else if (cursors.right.isDown) {
-      console.log('RIGHT');
-      this.jeff.setVelocity(this.#VELOCITY_FACTOR * 1, 0);
+      this.EventBus.emit('ARROW_RIGHT');
     } else if (cursors.up.isDown) {
-      console.log('UP');
-      this.jeff.setVelocity(0, this.#VELOCITY_FACTOR * -1);
+      this.EventBus.emit('ARROW_UP');
     } else if (cursors.down.isDown) {
-      console.log('DOWN');
-      this.jeff.setVelocity(0, this.#VELOCITY_FACTOR * 1);
+      this.EventBus.emit('ARROW_DOWN');
     }
   }
 }
